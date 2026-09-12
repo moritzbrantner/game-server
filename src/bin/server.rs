10d1908a346727afcafa5d5f0c7c3e10bfc096f2
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-12 08:21:47 +0200

    Apply canonical pluggable-server formatting

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -11,12 +11,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .ok()
         .and_then(|value| value.parse::<u16>().ok())
         .unwrap_or(DEFAULT_PORT);
-    let certificate_pem = PathBuf::from(
-        env::var("GAME_SERVER_CERT_PEM").unwrap_or_else(|_| "cert.pem".to_owned()),
-    );
-    let private_key_pem = PathBuf::from(
-        env::var("GAME_SERVER_KEY_PEM").unwrap_or_else(|_| "key.pem".to_owned()),
-    );
+    let certificate_pem =
+        PathBuf::from(env::var("GAME_SERVER_CERT_PEM").unwrap_or_else(|_| "cert.pem".to_owned()));
+    let private_key_pem =
+        PathBuf::from(env::var("GAME_SERVER_KEY_PEM").unwrap_or_else(|_| "key.pem".to_owned()));
     let session_path = env::var("GAME_SERVER_SESSION_PATH").unwrap_or_else(|_| "/game".to_owned());
 
     serve(
